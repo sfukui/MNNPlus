0843ed3a1970ed9ac7686aca6035f12b30e52252
Fukui Shogo <sfukui07@gmail.com>
2016-08-25 06:33:13 +0900

    [Fix] Some settings in unit test.
    Fixed a version conflict in "NUnit.framework.dll" reference.
    Reset unit tests using "Timeout" attribute.

--- src/UnitTests/LinearAlgebraProviderTests/Complex/LinearAlgebraProviderTests.cs
+++ src/UnitTests/LinearAlgebraProviderTests/Complex/LinearAlgebraProviderTests.cs
@@ -1093,7 +1093,7 @@ namespace MathNet.Numerics.UnitTests.LinearAlgebraProviderTests.Complex
 
         [TestCase("Wide10x50000", "Tall50000x10")]
         [TestCase("Square1000x1000", "Square1000x1000")]
-//        [Explicit, Timeout(1000*10)]
+        [Explicit, Timeout(1000*10)]
         public void IsMatrixMultiplicationPerformant(string leftMatrixKey, string rightMatrixKey)
         {
             var leftMatrix = _matrices[leftMatrixKey];
